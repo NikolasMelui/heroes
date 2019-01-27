e2dd6212a0480ca7c918babd72f9dfcf37f9261b
nikolasmelui <slak@samaradom.ru>
2019-01-27 17:05:52 +0400

    Add new planet model entity to the app

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,2 @@
 export * from './hero.model';
+export * from './planet.model';
---
+++ src/models/planet.model.ts
@@ -0,0 +1,22 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Planet extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    required: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+
+  constructor(data?: Partial<Planet>) {
+    super(data);
+  }
+}
